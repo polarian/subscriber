3547cd95a782cae32b27a971fbab54d007f3072c
polarian <stpolarian@gmail.com>
2018-07-24 00:33:18 +0900

    manually configuring lettuce.

--- src/main/kotlin/io/polarian/subscriber/RedisConfig.kt
+++ src/main/kotlin/io/polarian/subscriber/RedisConfig.kt
@@ -1,25 +1,49 @@
 package io.polarian.subscriber
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.listener.ChannelTopic
 import org.springframework.data.redis.listener.RedisMessageListenerContainer
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter
-import org.springframework.data.redis.listener.ChannelTopic
-import java.util.concurrent.Executor
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
+
+class RedisProperties() {
+    lateinit var host:String
+    lateinit var port:String
+    lateinit var password:String
+}
 
 @Configuration
 class RedisConfig {
-    @Bean
+
+    @Bean("myProperties")
+    @ConfigurationProperties(prefix="my.redis")
+    fun getRedisProperties():RedisProperties {
+        return RedisProperties()
+    }
+
+    @Bean("myExecutor")
     @ConfigurationProperties(prefix = "my.pool")
     fun taskExecutor():Executor {
         return ThreadPoolTaskExecutor()
     }
 
-    @Bean
-    fun redisContainer(redisConnectionFactory: RedisConnectionFactory, executor : Executor): RedisMessageListenerContainer {
+    @Bean("myConnectionFactory")
+    fun redisConnectionFactory(@Qualifier("myProperties") redisProperties: RedisProperties): RedisConnectionFactory {
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration(redisProperties.host, redisProperties.port.toInt())
+        redisStandaloneConfiguration.password = RedisPassword.of(redisProperties.password)
+        return  LettuceConnectionFactory(redisStandaloneConfiguration)
+    }
+
+    @Bean("myContainer")
+    fun redisContainer(@Qualifier("myConnectionFactory") redisConnectionFactory: RedisConnectionFactory, @Qualifier("myExecutor") executor : Executor): RedisMessageListenerContainer {
         val container = RedisMessageListenerContainer()
         container.setConnectionFactory(redisConnectionFactory)
         container.addMessageListener(MessageListenerAdapter(RedisMessageSubscriber()), ChannelTopic("MSG"))
